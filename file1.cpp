1fa9e3171de971dd6fba8741f6e740631b0f8599
刘子健_台式 <alphatk@vip.qq.com>
2020-12-23 10:58:24 +0800

    20201223

---
+++ file1.cpp
@@ -0,0 +1,15 @@
+/*input
+3
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    vector<int> myi;
+    for(int i=0;i<t;i++)
+        myi.push_back(i);
+    for(int i=0;i<t;i++)
+        cout<<myi[i];
+    return 0;
+}
